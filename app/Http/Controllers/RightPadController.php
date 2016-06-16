04f27f7507f4cc495e35ec1600145acc5b4fe9a1
Vultour <Vultour@users.noreply.github.com>
2016-06-16 02:32:55 +0200

    Add Right-pad service

---
+++ app/Http/Controllers/RightPadController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers;
+
+
+class RightPadController extends ApiController
+{
+    public function getDefault($input, $length){
+        if (!ctype_digit($length) || !is_numeric($length)){ return $this->error('Length must be an integer'); }
+        if (($length > 4096) || (strlen($input) > 4096)){ return $this->error('Input size too large, please contact us for pricing'); }
+
+        $output = $input;
+        while (strlen($output) < $length){ $output .= " "; }
+
+        return $this->success($output);
+    }
+
+    public function getCustom($input, $length, $char){
+        if (!ctype_digit($length) || !is_numeric($length)){ return $this->error('Length must be an integer'); }
+        if (strlen($char) != 1){ return $this->error('You must specift a single character for padding'); }
+        if (($length > 4096) || (strlen($input) > 4096)){ return $this->error('Input size too large, please contact us for pricing'); }
+
+        $output = $input;
+        while (strlen($output) < $length){ $output .= $char; }
+    
+        return $this->success($output);
+    }
+}
