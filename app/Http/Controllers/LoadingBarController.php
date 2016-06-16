29638aa9d1c9da3e43a09a0110ebaf99e4f63a17
Vultour <Vultour@users.noreply.github.com>
2016-06-16 13:56:00 +0200

    Add more validation

--- app/Http/Controllers/LoadingBarController.php
+++ app/Http/Controllers/LoadingBarController.php
@@ -19,19 +19,22 @@ class LoadingBarController extends ApiController
     }
 
     public function getDefault($done){
-        if (($done < 0) || ($done > 100)){ return $this->error('Done % has to be between 0 and 100'); }
+        if (!ctype_digit($done)){ return $this->error('Done% needs to be an integer'); }
+        if (($done < 0) || ($done > 100)){ return $this->error('Done% has to be between 0 and 100'); }
         return $this->success($this->generateLoadingBar($done, 20, '=', '>', '|'));
     }
 
     public function getLength($done, $length){
-        if (($done < 0) || ($done > 100)){ return $this->error('Done % has to be between 0 and 100'); }
-        if (($length < 1) || ($length > 4096)){ return $this->error('Invalid length, allowed values are between 1 and 4096, contact us for pricing if you need larger loading bars'); }
+        if ((!ctype_digit($done)) || (!ctype_digit($length))){ return $this->error('Done% and Length need to be integers'); }
+        if (($done < 0) || ($done > 100)){ return $this->error('Done% has to be between 0 and 100'); }
+        if (($length < 1) || ($length > 4096)){ return $this->error('Invalid Length, allowed values are between 1 and 4096, contact us for pricing if you need larger loading bars'); }
         return $this->success($this->generateLoadingBar($done, $length, '=', '>', '|'));
     }
 
     public function getCustom($done, $length, $loadedChar, $loadingChar, $borderChar){
-        if (($done < 0) || ($done > 100)){ return $this->error('Done % has to be between 0 and 100'); }
-        if (($length < 1) || ($length > 4096)){ return $this->error('Invalid length, allowed values are between 1 and 4096, contact us for pricing if you need larger loading bars'); }
+        if ((!ctype_digit($done)) || (!ctype_digit($length))){ return $this->error('Done% and Length need to be integers'); }
+        if (($done < 0) || ($done > 100)){ return $this->error('Done% has to be between 0 and 100'); }
+        if (($length < 1) || ($length > 4096)){ return $this->error('Invalid Length, allowed values are between 1 and 4096, contact us for pricing if you need larger loading bars'); }
         if ((strlen($loadedChar) != 1) || (strlen($loadingChar) != 1) || (strlen($borderChar) != 1)){ return $this->error('You must only specify 1 character for each custom character'); }
 
         return $this->success($this->generateLoadingBar($done, $length, $loadedChar, $loadingChar, $borderChar));
