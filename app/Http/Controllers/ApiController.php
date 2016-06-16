6b03814b9f0d147c069c1eb80390cf0ae1f4d516
Vultour <Vultour@users.noreply.github.com>
2016-06-16 03:44:47 +0200

    Add additional Content-Type options

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,13 +1,52 @@
 <?php
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 
 class ApiController extends Controller
 {
+    protected $serviceTitle = 'Service';
+    protected $request;
+
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+
+    protected function htmlify($input){
+        $output = '<ul>';
+        foreach ($input as $key => $value){
+            $output .= "<li style='margin-left:15px;'><strong>$key</strong>: ";
+            if (is_array($value)){
+                $output .= $this->htmlify($value);
+            } else{
+                $output .= $value;
+            }
+            $output .= '</li>';
+        }
+        $output .= '</ul>';
+        return $output;
+    }
+
     protected function createResponse($output, $status){
         // TODO
-        return response()
-                ->json(['status' => $status, 'data' => $output]);
+        $accept = $this->request->header('Accept');
+
+        if (strpos($accept, 'text/html') !== FALSE){
+            $htmlOutput = "";
+            if (is_array($output)){
+                $htmlOutput = $this->htmlify($output);
+            } else{
+                $htmlOutput = $output;
+            }
+            return view('output', ['output' => $htmlOutput, 'serviceTitle' => $this->serviceTitle]);
+        } else if (strpos($accept, 'text/plain') !== FALSE){
+            return response($output, $status)
+                    ->header('Content-Type', 'text/plain');
+        } else{
+            return response()
+                    ->json(['status' => $status, 'data' => $output]);
+        }
     }
 
     protected function success($output){ return $this->createResponse($output, 200); }
--- resources/views/output.php
+++ resources/views/output.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>AaaS</title>
+        <title>MTaaS</title>
         <link rel="stylesheet" href="/css/materialize.min.css">
     </head>
     <body>
         <div class="row container">
-            <div class="col s6 offset-s3 valign-wrapper">
-                <div class="card blue-grey darken-1 valign">
+            <div class="col s6 offset-s3">
+                <div class="card blue-grey darken-1">
                     <div class="card-content white-text">
                         <span class="card-title"><?php echo $serviceTitle; ?></span>
                         <p>
@@ -15,7 +15,7 @@
                         </p>
                     </div>
                     <div class="card-action">
-                    <a href="/">Anything-as-a-Service</a>
+                    <a href="/">Many-Things-as-a-Service</a>
                     </div>
                 </div>
             </div>
