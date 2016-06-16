43d57c70f7ed5f994204975ae9ab4f617dd8c8b9
Vultour <Vultour@users.noreply.github.com>
2016-06-16 02:32:42 +0200

    Add Left-pad service

--- app/Http/Controllers/LeftPadController.php
+++ app/Http/Controllers/LeftPadController.php
@@ -2,14 +2,26 @@
 namespace App\Http\Controllers;
 
 
-class ExampleController extends ApiController
+class LeftPadController extends ApiController
 {
-    public function __construct()
-    {
-        //
+    public function getDefault($input, $length){
+        if (!ctype_digit($length) || !is_numeric($length)){ return $this->error('Length must be an integer'); }
+        if (($length > 4096) || (strlen($input) > 4096)){ return $this->error('Input size too large, please contact us for pricing'); }
+
+        $output = $input;
+        while (strlen($output) < $length){ $output = " " . $output; }
+
+        return $this->success($output);
     }
 
-    public function getDefault(){
-        return $this->success('example');
+    public function getCustom($input, $length, $char){
+        if (!ctype_digit($length) || !is_numeric($length)){ return $this->error('Length must be an integer'); }
+        if (strlen($char) != 1){ return $this->error('You must specift a single character for padding'); }
+        if (($length > 4096) || (strlen($input) > 4096)){ return $this->error('Input size too large, please contact us for pricing'); }
+
+        $output = $input;
+        while (strlen($output) < $length){ $output = $char . $output; }
+
+        return $this->success($output);
     }
 }
