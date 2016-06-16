64d4f1e92606e2181e9cca04bac98e44ec53af91
Vultour <Vultour@users.noreply.github.com>
2016-06-16 13:55:36 +0200

    First release version of homepage

--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -14,7 +14,7 @@
         <nav class="light-blue lighten-1" role="navigation">
             <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">MTaaS</a>
                 <ul class="right">
-                    <li><a href="#">Something</a></li>
+                    <li><a href="#microservices">List</a></li>
                 </ul>
             </div>
         </nav>
@@ -27,7 +27,7 @@
                     <h5 class="header col s12 light">Your final stop for microservices</h5>
                 </div>
                 <div class="row center">
-                    <a href="#" id="start" class="btn-large orange">Get Started</a>
+                    <a href="#microservices" id="start" class="btn-large orange">Get Started</a>
                 </div>
                 <br><br>
             </div>
@@ -39,23 +39,23 @@
                 <div class="row">
                     <div class="col s12 m4">
                         <div class="icon-block">
-                            <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+                            <h2 class="center light-blue-text"><i class="material-icons large">flash_on</i></h2>
                             <h5 class="center">Speeds up development</h5>
-                            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                            <p class="light">Are you getting lost in the plethora of microservice providers you need to use to bring your next big idea to life? Or maybe you just have to use that one esoteric language that requires you to write 100 lines of code to generate a <a href="#random">random</a> number. You don't need to worry anymore, we are here to provide the largest suite of microservices in one conveniently priced package.</p>
                         </div>
                     </div>
                     <div class="col s12 m4">
                         <div class="icon-block">
-                            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-                            <h5 class="center">User Experience Focused</h5>
-                            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                            <h2 class="center light-blue-text"><i class="material-icons large">grade</i></h2>
+                            <h5 class="center">Community-approved</h5>
+                            <p class="light">To remain relevant, we decided to <a href="https://github.com/Vultour/many-things-as-a-service">open-source</a> our platform like many other popular projects do these days. This enables us to have a bug-free, community-approved codebase without much work from our side.</p>
                         </div>
                     </div>
                     <div class="col s12 m4">
                         <div class="icon-block">
-                            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-                            <h5 class="center">Easy to work with</h5>
-                            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                            <h2 class="center light-blue-text"><i class="material-icons large">http</i></h2>
+                            <h5 class="center">Suited for everyone</h5>
+                            <p class="light">All of our endpoints support multiple output styles based on your <em>Accept:</em> header. These include <em>text/html</em>, <em>text/plain</em>, and the default <em>text/json</em>. No matter what needs you migth have, rest assured we have the right output for you.</p>
                         </div>
                     </div>
                 </div>
@@ -65,9 +65,9 @@
 
 
         <div class="container">
-            <h3 class="center" id="microservices">Available microservices</h2>
-            <?php foreach ($services as $service): ?>
-            <div class="section">
+            <h3 class="center" id="microservices">Available microservices</h3>
+            <?php foreach ($services as $stub => $service): ?>
+            <div class="section" id="<?php echo $stub; ?>">
                 <div class="row">
                     <div class="col s4">
                     <h4><?php echo $service['title']; ?></h4>
@@ -94,20 +94,20 @@
                 <div class="row">
                     <div class="col l6 s12">
                         <h5 class="white-text">Company Bio</h5>
-                        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                        <p class="grey-text text-lighten-4">We are a group of visionaries with an impressive record of failed start-ups. Many-Things-as-a-Service has been born from the idea that we could steal from all of our competitors at once, and offer the result at a heftily marked-up price.</p>
                     </div>
                     <div class="col l3 s12">
-                        <h5 class="white-text">Settings</h5>
+                        <h5 class="white-text">Contact</h5>
                         <ul>
-                            <li><a class="white-text" href="#!">Link 1</a></li>
-                            <li><a class="white-text" href="#!">Link 2</a></li>
+                            <li><a class="white-text" href="https://github.com/Vultour/many-things-as-a-service">Enterprise enquiries</a></li>
+                            <li><a class="white-text" href="https://github.com/Vultour/many-things-as-a-service">Found a bug?</a></li>
                         </ul>
                     </div>
                     <div class="col l3 s12">
-                        <h5 class="white-text">Connect</h5>
+                        <h5 class="white-text">Careers</h5>
                         <ul>
-                            <li><a class="white-text" href="#!">Link 1</a></li>
-                            <li><a class="white-text" href="#!">Link 2</a></li>
+                            <li><a class="white-text" href="https://github.com/Vultour/many-things-as-a-service">Open positions</a></li>
+                            <li><a class="white-text" href="https://github.com/Vultour/many-things-as-a-service">Working at MTaaS</a></li>
                         </ul>
                     </div>
                 </div>
